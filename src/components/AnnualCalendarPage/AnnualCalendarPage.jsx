b1087b6d172ffb9cb47109d1d5e48fc2c4322d1a
pwielgos <paigewielgos@gmail.com>
2020-03-06 11:27:06 -0600

    boilerplate template for teacher components

---
+++ src/components/AnnualCalendarPage/AnnualCalendarPage.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+// import Nav from '../Nav/Nav';
+// import Footer from '../Footer/Footer';
+// import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
+
+class AnnualCalendarPage extends Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Teacher Home Page - Annual Calendar</h1>
+
+
+            </div>
+        )
+    }
+}
+
+export default connect()(AnnualCalendarPage);
