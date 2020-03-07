c850e551cc2f5a4df01846cf90abf946e6fe3ef9
luke rohde <morneau33@gmail.com>
2020-03-06 20:22:32 -0600

    fixed nav bar fonts and case

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,27 +7,27 @@ import './Nav.css';
 const Nav = (props) => (
   <div className="nav">
     <Link to="/home">
-      <h2 className="nav-title">Creatively Focused</h2>
+      <h2 className="nav-title">CREATIVELY FOCUSED</h2>
     </Link>
     <div className="nav-right">
       <Link className="nav-link" to="/home">
         {/* Show this link if they are logged in or not,
         but call this link 'Home' if they are logged in,
         and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Home' : 'Login / Register'}
+        {props.user.id ? 'HOME' : 'LOGIN / REGISTER'}
       </Link>
       {/* Show the link to the info page and the logout button if the user is logged in */}
       {props.user.id && (
         <>
           <Link className="nav-link" to="/info">
-            Info Page
+            INFO PAGE
           </Link>
           <LogOutButton className="nav-link"/>
         </>
       )}
       {/* Always show this link since the about page is not protected */}
       <Link className="nav-link" to="/about">
-        About
+        ABOUT
       </Link>
     </div>
   </div>
