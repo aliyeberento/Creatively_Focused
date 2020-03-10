be8163c7ffc4389d642962a90eabe1c237bb78ce
luke rohde <morneau33@gmail.com>
2020-03-10 11:19:47 -0500

    gabes masterpiece, edit student state now populated, form populated from local state

--- src/components/TeacherComponents/EditStudentPage/EditStudentPage.jsx
+++ src/components/TeacherComponents/EditStudentPage/EditStudentPage.jsx
@@ -6,32 +6,29 @@ import { connect } from 'react-redux';
 
 class EditStudentPage extends Component {
 
-    state = {
-        student: {
-            firstname: this.props.student.firstname,
-            lastname: '',
-            grade: '',
-            student_id: '',
-            previousIep: '',
-            previousEval: '',
-            disabilityCategory: '',
-            federalSetting: '',
-            birthdate: '',
-            notes: '',
-            teacher: '',
-            schoolId: '',
-            isdId: ''
-        }
+    componentDidMount() {
+        // let selectedStudent = this.props.student.filter(student => student.id == this.props.match.params.id)[0];
+        this.props.dispatch({type: 'GET_STUDENT',})
     }
 
-    componentDidMount() {
-        this.props.dispatch({type: 'GET_STUDENT',})
+    state = {
+        student: {},
+        renderedOnce: false
     }
 
     updateStudent = (event, propertyValue) => {
         console.log('making a new student in state', this.state.student);
         // this function should spread local state
         // update the student object in local
+            this.setState({
+                renderedOnce:true
+            }, () => {
+                this.setState({
+                    student: {...this.props.student.filter(student => student.id == this.props.match.params.id)[0]}
+                })
+            })
+        
+        
     }
 
     submitStudent = () => {
@@ -42,28 +39,28 @@ class EditStudentPage extends Component {
     }
 
     render() {
-        console.log(this.props.student);
-        console.log(this.props.match.params.id);
-        console.log(this.state.student);
-        console.log('student reducer contents:', this.props.student);
-        console.log(this.props.student.filter(student => student.id == this.props.match.params.id));
+        // console.log(this.props.student);
+        // console.log(this.props.match.params.id);
+        console.log(this.state);
+        // console.log('student reducer contents:', this.props.student);
+        // console.log(this.props.student.filter(student => student.id == this.props.match.params.id));
         let selectedStudent = this.props.student.filter(student => student.id == this.props.match.params.id)[0];
         console.log(selectedStudent);        
-        // let studentToEdit = this.props.student.filter(student => student.id == this.props.match.params.id)[0];
         return (
             <div>
                 <h1>Edit Student</h1>
                 <form>
-                    <label>First Name: <input type="text" placeholder="first name" defaultValue={selectedStudent && selectedStudent.firstname}></input></label><br />
-                    <label>Last Name: <input type="text" placeholder="last name" defaultValue={selectedStudent && selectedStudent.lastname}></input></label><br />
-                    <label>Grade: <input type="number" placeholder="grade" defaultValue={selectedStudent && selectedStudent.grade}></input></label><br />
-                    <label>Student ID: <input type="number" placeholder="id number" defaultValue={selectedStudent && selectedStudent.student_id}></input></label><br />
-                    <label>Next IEP: <input type="date" placeholder="next iep date" defaultValue={selectedStudent && selectedStudent.next_iep}></input></label><br />
-                    <label>Next EVAL: <input type="date" placeholder="next eval date" defaultValue={selectedStudent && selectedStudent.next_eval}></input></label><br />
-                    <label>Disability Category: <input type="number" placeholder="disability category" defaultValue={selectedStudent && selectedStudent.disability_cat}></input></label><br />
-                    <label>Federal Setting: <input type="number" placeholder="federal setting" defaultValue={selectedStudent && selectedStudent.fed_setting}></input></label><br />
-                    <label>Date of Birth: <input type="date" placeholder="birthdate" defaultValue={selectedStudent && selectedStudent.birthdate}></input></label><br />
-                    <label>Student Notes: <input type="text" placeholder="notes" defaultValue={selectedStudent && selectedStudent.notes}></input></label><br />
+                    {selectedStudent && !this.state.renderedOnce ? this.updateStudent() : <></>}
+                    <label>First Name: <input type="text" placeholder="first name" defaultValue={this.state.student.firstname}></input></label><br />
+                    <label>Last Name: <input type="text" placeholder="last name" defaultValue={this.state.student.lastname}></input></label><br />
+                    <label>Grade: <input type="number" placeholder="grade" defaultValue={this.state.student.grade}></input></label><br />
+                    <label>Student ID: <input type="number" placeholder="id number" defaultValue={this.state.student.student_id}></input></label><br />
+                    <label>Next IEP: <input type="date" placeholder="next iep date" defaultValue={this.state.student.next_iep}></input></label><br />
+                    <label>Next EVAL: <input type="date" placeholder="next eval date" defaultValue={this.state.student.next_eval}></input></label><br />
+                    <label>Disability Category: <input type="number" placeholder="disability category" defaultValue={this.state.student.disability_cat}></input></label><br />
+                    <label>Federal Setting: <input type="number" placeholder="federal setting" defaultValue={this.state.student.fed_setting}></input></label><br />
+                    <label>Date of Birth: <input type="date" placeholder="birthdate" defaultValue={this.state.student.birthdate}></input></label><br />
+                    <label>Student Notes: <input type="text" placeholder="notes" defaultValue={this.state.student.notes}></input></label><br />
                     <button onClick={this.submitStudent}>update student</button>
                 </form>
             </div>
