c880676259cc571ba92629dbb13e45bf7370346d
luke rohde <morneau33@gmail.com>
2020-03-09 20:58:24 -0500

    student list going to detail going to edit

--- src/components/TeacherComponents/StudentDetailPage/StudentDetailPage.jsx
+++ src/components/TeacherComponents/StudentDetailPage/StudentDetailPage.jsx
@@ -26,19 +26,20 @@ class StudentDetailPage extends Component {
             <div>
                 <h1>Student Detail Page</h1>
                 <ul>
-                    <li>{selectedStudent && selectedStudent.firstname}</li>
-                    <li>{selectedStudent && selectedStudent.lastname}</li>
-                    <li>{selectedStudent && selectedStudent.grade}</li>
-                    <li>{selectedStudent && selectedStudent.disability_cat}</li>
-                    <li>{selectedStudent && selectedStudent.fed_setting}</li>
-                    <li>{selectedStudent && selectedStudent.student_id}</li>
-                    <li>{selectedStudent && selectedStudent.initial_iep}</li>
-                    <li>{selectedStudent && selectedStudent.prev_iep}</li>
-                    <li>{selectedStudent && selectedStudent.next_iep}</li>
-                    <li>{selectedStudent && selectedStudent.prev_eval}</li>
-                    <li>{selectedStudent && selectedStudent.teacher}</li>
-                    <li>{selectedStudent && selectedStudent.school_id}</li>
-                    <li>{selectedStudent && selectedStudent.isd_id}</li>
+                    <li>First Name: {selectedStudent && selectedStudent.firstname}</li>
+                    <li>Last Name: {selectedStudent && selectedStudent.lastname}</li>
+                    <li>Grade: {selectedStudent && selectedStudent.grade}</li>
+                    <li>Disability Category: {selectedStudent && selectedStudent.disability_cat}</li>
+                    <li>Federal Setting: {selectedStudent && selectedStudent.fed_setting}</li>
+                    <li>Student ID: {selectedStudent && selectedStudent.student_id}</li>
+                    <li>Initial IEP: {selectedStudent && selectedStudent.initial_iep}</li>
+                    <li>Last IEP: {selectedStudent && selectedStudent.prev_iep}</li>
+                    <li>Next IEP: {selectedStudent && selectedStudent.next_iep}</li>
+                    <li>Last EVAL: {selectedStudent && selectedStudent.prev_eval}</li>
+                    <li>Next EVAL: {selectedStudent && selectedStudent.next_eval}</li>
+                    <li>Teacher: {selectedStudent && selectedStudent.teacher}</li>
+                    <li>School: {selectedStudent && selectedStudent.school_id}</li>
+                    <li>District: {selectedStudent && selectedStudent.isd_id}</li>
                 </ul>
                 <button onClick={() => this.goToEdit(selectedStudent.id)}>EDIT STUDENT</button>
                 <br />
