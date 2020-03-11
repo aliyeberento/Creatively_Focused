c2e5b8587afb77b55fadf016efa4b479ce8d5936
pwielgos <paigewielgos@gmail.com>
2020-03-11 15:48:56 -0500

    get route w/sql query working

--- server/modules/mailUsersNotifications.js
+++ server/modules/mailUsersNotifications.js
@@ -2,23 +2,8 @@ const pool = require('./pool');
 const sendEmail = require('./mailer');
 
 //email all users their daily deadline reminders
-module.exports = async function () {
+module.exports = () => {
     console.log('firing');
-    let currentDate = `SELECT CURRENT_DATE;`
-    const selectUsers = `
-        SELECT "user"."id" AS "user_id",
-        "user"."username",
-        "user"."prefcomm",
-        "student"."firstname",
-        "student"."lastname",
-        "student_event"."due_date",
-        "student_event"."completed",
-        "event"."task",
-        "event"."notes"
-        FROM "user"
-        INNER JOIN "student" ON "student"."teacher" = "user"."id"
-        INNER JOIN "student_event" ON "student"."id" = "student_event"."student_id"
-        INNER JOIN "event" ON "student_event"."event_id" = "event"."id"
-        WHERE "user"."id"=$1 AND "student_event"."completed"=false AND "student_event"."due_date"=${currentDate};`
+    //moved query to a router ??
 
 }
--- server/modules/mailer.js
+++ server/modules/mailer.js
@@ -2,28 +2,31 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 const cron = require('cron');
+const sendMailTo = require('../modules/mailer');
 
 // step 1 
 // email transport configuration
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD
     }
 });
 
-
-module.exports = () => {
-    cron.schedule("* * 1 * *", () => {
+module.exports = (toDos) => {
+    cron.schedule('* 24 * * *', () => {
         // step 2
         // email message options
         const mailOptions = {
             from: process.env.EMAIL, // sender address
-            to: email, // list of receivers
+            to: 'paigewielgos@gmail.com', // list of receivers
             subject: 'test', // Subject line
-            text: textContents,
-            html: htmlContents || undefined
+            text: 'test',
+            html: '<p>test</p>'
         }
         // step 3
         // send email
