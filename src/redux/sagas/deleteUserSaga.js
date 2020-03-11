b74e6d37b8d57970e5670d068816904cd2322f17
luke rohde <morneau33@gmail.com>
2020-03-11 12:31:14 -0500

    full CRUD on users

---
+++ src/redux/sagas/deleteUserSaga.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { put, takeLatest } from 'redux-saga/effects';
+
+function* deleteUser(action) {
+    console.log(action);
+    try {
+        let id = action.payload
+        console.log(action.payload)
+        let response = yield axios.delete(`/api/teacherList/${id}`);
+        yield put({ type: 'GET_TEACHERS' })
+
+    }
+    catch (error) {
+        console.log('Error in delete user saga', error)
+    }
+}
+
+function* deleteUserSaga() {
+    yield takeLatest('DELETE_USER', deleteUser);
+}
+
+export default deleteUserSaga;
