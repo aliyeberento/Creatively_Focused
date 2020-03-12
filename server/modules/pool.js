897c3eb78c25a2641f565cd94a8eb4f2997dddb6
Ken Slack <kenslack95@gmail.com>
2020-03-12 16:55:00 -0500

    changed calendar

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -24,7 +24,7 @@ if (process.env.DATABASE_URL) {
     database: params.pathname.split('/')[1],
     ssl: true, // heroku requires ssl to be true
     max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    idleTimeoutMillis: 90000, // how long a client is allowed to remain idle before being closed
   };
 } else {
   config = {
@@ -32,7 +32,7 @@ if (process.env.DATABASE_URL) {
     port: 5432, // env var: PGPORT
     database: 'creatively-focused', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
     max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    idleTimeoutMillis: 90000, // how long a client is allowed to remain idle before being closed
   };
 }
 
--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './user.css';
-import Calendar from 'react-calendar';
+//import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import Moment from 'react-moment';
+import {
+  Calendar,
+  momentLocalizer,
+} from 'react-big-calendar';
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+const localizer = momentLocalizer(moment);
+
+
 // import LogOutButton from '../LogOutButton/LogOutButton';
 
 // this could also be written with destructuring parameters as:
@@ -20,6 +30,18 @@ class UserPage extends Component {
     user: {
       // this is where the user's info is held locally
     },
+    events: [
+      {
+        start: new Date(),
+        end: new Date(moment().add(1, "days")),
+        title: "Some title"
+      },
+      {
+        start: new Date(),
+        end: new Date(moment().add(1, "days")),
+        title: "new thing"
+      }
+    ],
     date: new Date(),
   
   }
@@ -28,43 +50,65 @@ class UserPage extends Component {
     this.props.dispatch({ type: 'GET_STUDENTEVENT' });
   }
 
-  onChange = date => this.setState({ date })
+  onChange = (date) => {
+    this.setState({ date });
+    console.log('on change triggered')
+  }
+  
 
   editUser = () => {
     console.log('editing THIS user:', this.props.user.username); 
   }
 
   render() {
-    console.log(this.props.user.username);
+    console.log(this.state.date);
     
     return (
       <div className="welcome">
         <h1 >
           Welcome, {this.props.user.username}!
         </h1>
-        <Calendar
+        {/* <Calendar
           onChange={this.onChange}
           value={this.state.date}
+        /> */}
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{ height: "100vh" }}
+          onChange={this.onChange}
+          value={this.onChange}
         />
+        {console.log(this.state.events)}
+        
         <table>
           <thead>
             <tr>
               <th>
                 Task
                   </th>
-              <th>Notes</th>
+              <th>Date</th>
             </tr>
           </thead>
           <tbody>
           {this.props.studentEvent.map(event => (
             <tr key={event.id}>
+              
               <td>{event.task}</td>
-              <td>{event.notes}</td>
+              <td>
+                <Moment id="marker" format="MM-D-YYYY" >{event.date}</Moment>
+                
+              </td>
+              
             </tr>
           )
           )}
+          
           </tbody>
         </table>
+        <h3 id="marker">!</h3>
         <button onClick={this.editUser}>EDIT USER PROFILE</button>
         {/* <LogOutButton className="log-in" /> */}
       </div>
