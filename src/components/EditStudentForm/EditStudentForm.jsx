58ddf4df8ec12beaa9dbb7b5d3947fdf2ae9f33d
luke rohde <morneau33@gmail.com>
2020-03-12 11:29:09 -0500

    edit student form filled in

--- src/components/EditStudentForm/EditStudentForm.jsx
+++ src/components/EditStudentForm/EditStudentForm.jsx
@@ -72,7 +72,71 @@ class EditStudentForm extends Component {
                             defaultValue={this.props.reduxState.studentDetail.firstname}
                             onChange={(event) => this.updateStudent(event, 'firstname')}
                         />
-                    </label>
+                    </label><br />
+                    <label>Last Name:
+                        <input
+                            type="text"
+                            placeholder="last name"
+                            defaultValue={this.props.reduxState.studentDetail.lastname}
+                            onChange={(event) => this.updateStudent(event, 'lastname')}
+                        />
+                    </label><br />
+                    <label>Grade:
+                        <input
+                            type="text"
+                            placeholder="grade"
+                            defaultValue={this.props.reduxState.studentDetail.grade}
+                            onChange={(event) => this.updateStudent(event, 'grade')}
+                        />
+                    </label><br />
+                    <label>Date of Birth:
+                        <input
+                            type="date"
+                            placeholder="birthdate"
+                            defaultValue={this.props.reduxState.studentDetail.birthdate}
+                            onChange={(event) => this.updateStudent(event, 'birthdate')}
+                        />
+                    </label><br />
+                    <label>Next IEP:
+                        <input
+                            type="date"
+                            placeholder="next iep"
+                            defaultValue={this.props.reduxState.studentDetail.next_iep}
+                            onChange={(event) => this.updateStudent(event, 'next_iep')}
+                        />
+                    </label><br />
+                    <label>Next EVAL:
+                        <input
+                            type="date"
+                            placeholder="next_eval"
+                            defaultValue={this.props.reduxState.studentDetail.next_eval}
+                            onChange={(event) => this.updateStudent(event, 'next_eval')}
+                        />
+                    </label><br />
+                    <label>Disability Category:
+                        <input
+                            type="number"
+                            placeholder="disability category"
+                            defaultValue={this.props.reduxState.studentDetail.disability_cat}
+                            onChange={(event) => this.updateStudent(event, 'disability_cat')}
+                        />
+                    </label><br />
+                    <label>Federal Setting:
+                        <input
+                            type="number"
+                            placeholder="federal setting"
+                            defaultValue={this.props.reduxState.studentDetail.fed_setting}
+                            onChange={(event) => this.updateStudent(event, 'fed_setting')}
+                        />
+                    </label><br />
+                    <label>Notes:
+                        <input
+                            type="text"
+                            placeholder="notes"
+                            defaultValue={this.props.reduxState.studentDetail.nptes}
+                            onChange={(event) => this.updateStudent(event, 'nptes')}
+                        />
+                    </label><br />
 
                     {/* <input type="text" label="name" defaultValue={student.firstname} value={student.firstname} onChange={(event) => this.editThisStudent(event, 'firstname')}></input><br />
                         <input type="text" label="name" defaultValue={student.lastname} value={student.lastname} onChange={(event) => this.editThisStudent(event, 'lastname')}></input><br />
@@ -83,8 +147,8 @@ class EditStudentForm extends Component {
                         <input type="text" label="name" defaultValue={student.fed_setting} value={student.fed_setting} onChange={(event) => this.editThisStudent(event, 'fed_setting')}></input><br />
                         <input type="text" label="name" defaultValue={student.birthdate} value={student.birthdate} onChange={(event) => this.editThisStudent(event, 'birthdate')}></input><br />
                         <input type="text" label="name" defaultValue={student.notes} value={student.notes} onChange={(event) => this.editThisStudent(event, 'notes')}></input><br /> */}
+                    <button onClick={this.submitEdit}>submit changes</button>
                 </form>
-                <button onClick={this.submitEdit}>submit changes</button>
             </div >
         )
     }
