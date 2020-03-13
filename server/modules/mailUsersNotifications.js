960b6544752c0517c43cbca86507c57db628070e
pwielgos <paigewielgos@gmail.com>
2020-03-13 16:47:46 -0500

    working on cron job

--- server/modules/mailUsersNotifications.js
+++ server/modules/mailUsersNotifications.js
@@ -2,8 +2,22 @@ const pool = require('./pool');
 const sendEmail = require('./mailer');
 
 //email all users their daily deadline reminders
-module.exports = () => {
-    console.log('firing');
-    //moved query to a router ??
+//in progress - testing out cron to trigger emails rather than a GET route
+module.exports = async () => {
+    const selectUsers = `
+    SELECT "user"."id" AS "user_id",
+    "user"."username",
+    "user"."prefcomm",
+    "student"."firstname",
+    "student"."lastname",
+    "student_event"."due_date",
+    "student_event"."completed",
+    "event"."task",
+    "event"."notes"
+    FROM "user"
+    INNER JOIN "student" ON "student"."teacher" = "user"."id"
+    INNER JOIN "student_event" ON "student"."id" = "student_event"."student_id"
+    INNER JOIN "event" ON "student_event"."event_id" = "event"."id"
+    WHERE "student_event"."completed"=false AND "student_event"."due_date"=CURRENT_DATE;`
 
 }
--- server/modules/mailer.js
+++ server/modules/mailer.js
@@ -17,25 +17,52 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-module.exports = (toDos) => {
-    cron.schedule('* 24 * * *', () => {
-        // step 2
-        // email message options
-        const mailOptions = {
-            from: process.env.EMAIL, // sender address
-            to: 'paigewielgos@gmail.com', // list of receivers
-            subject: 'test', // Subject line
-            text: 'test',
-            html: '<p>test</p>'
-        }
-        // step 3
-        // send email
-        transporter.sendMail(mailOptions, function (err, info) {
-            if (err) {
-                console.log('err', err)
-            } else {
-                console.log('info', info);
+module.exports = () => {
+    cron.schedule('*/1 * * * *', () => {
+        const selectUsers = `
+        SELECT "user"."id" AS "user_id",
+        "user"."username",
+        "user"."prefcomm",
+        "student"."firstname",
+        "student"."lastname",
+        "student_event"."due_date",
+        "student_event"."completed",
+        "event"."task",
+        "event"."notes"
+        FROM "user"
+        INNER JOIN "student" ON "student"."teacher" = "user"."id"
+        INNER JOIN "student_event" ON "student"."id" = "student_event"."student_id"
+        INNER JOIN "event" ON "student_event"."event_id" = "event"."id"
+        WHERE "student_event"."completed"=false AND "student_event"."due_date"=CURRENT_DATE;`
+        let users = pool.query(selectUsers)
+        users = users.rows;
+            const transporter = nodemailer.createTransport({
+                host: 'smtp.gmail.com',
+                port: 587,
+                secure: false,
+                requireTLS: true,
+                auth: {
+                    user: process.env.EMAIL,
+                    pass: process.env.PASSWORD
+                }
+            });
+            for(i=0; i<results.rows.length; i++){
+            const mailOptions = {
+                from: process.env.EMAIL,
+                to: results.rows[i].username,
+                subject: 'test', 
+                text: 'test',
+                html: `<p>Looks like you have a(n) ${results.rows[i].task} on ${results.rows[i].due_date} coming up. 
+                I'll make sure you and the other team members are ready for this meeting by helping you space out the work. 
+                Let's get to work!</p>`
             }
-        });
+            transporter.sendMail(mailOptions, function (err, info) {
+                if (err) {
+                    console.log('err', err)
+                } else {
+                    console.log('info', info);
+                }
+            });
+        }
     });
 }
