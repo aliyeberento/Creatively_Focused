6990c284d902d57c28396273f6a53286d710b152
pwielgos <paigewielgos@gmail.com>
2020-03-17 12:45:18 -0500

    cron jobs getting triggered through async functions

--- server/modules/mailer.js
+++ server/modules/mailer.js
@@ -2,6 +2,7 @@
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 const cron = require('node-cron');
+const getNotifications = require('../modules/mailUsersNotifications');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -14,44 +15,31 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-// module.exports = (mailOptions) => {
-//     let userRows;
-//     const selectUsers = `
-//     SELECT "user"."id" AS "user_id",
-//     "user"."username",
-//     "user"."prefcomm",
-//     "student"."firstname",
-//     "student"."lastname",
-//     "student_event"."due_date",
-//     "student_event"."completed",
-//     "event"."task",
-//     "event"."notes"
-//     FROM "user"
-//     INNER JOIN "student" ON "student"."teacher" = "user"."id"
-//     INNER JOIN "student_event" ON "student"."id" = "student_event"."student_id"
-//     INNER JOIN "event" ON "student_event"."event_id" = "event"."id"
-//     WHERE "student_event"."completed"=false AND "student_event"."due_date"=CURRENT_DATE;`
-//     userRows = await pool.query(selectUsers);
-//     userRows = userRows.rows;
-//     for (i = 0; i < userRows.length; i++) {
-//         const mailOptions = {
-//             from: process.env.EMAIL,
-//             to: results.rows[i].username,
-//             subject: 'test',
-//             text: 'test',
-//             html: `<p>Looks like you have a(n) ${userRows[i].task} on ${userRows[i].due_date} coming up. 
-//                 I'll make sure you and the other team members are ready for this meeting by helping you space out the work. 
-//                 Let's get to work!</p>`
-//         }
-//     }
-// }
-    //Cron will run every morning at 8:00AM
-    cron.schedule('* 8 * * *', (mailOptions) => {
-        transporter.sendMail(mailOptions, function (err, info) {
-            if (err) {
-                console.log('err', err)
-            } else {
-                console.log('info', info);
-            }
-        });
-    });
+//email teachers their daily deadline reminders
+//Cron will run every morning at 8:00AM
+cron.schedule('* 8 * * *', async () => {  
+    const variable = await getNotifications();
+    for(i=0; i<variable.length; i++){
+        let email = variable[i].username;
+        let task = variable[i].task;
+        let dueDate = variable[i].due_date;
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: email,
+        subject: 'test',
+        text: 'test',
+        html:   `<p>Looks like you have a(n) ${task} on 
+                ${dueDate} coming up. I'll make sure you and the 
+                other team members are ready for this meeting by helping you 
+                space out the work. Let's get to work!</p>`
+    }
+    transporter.sendMail(mailOptions, function (err, info) {
+        if (err) {
+            console.log('err', err)
+        } else {
+            console.log('info', info);
+        }
+    })
+}
+});
+
